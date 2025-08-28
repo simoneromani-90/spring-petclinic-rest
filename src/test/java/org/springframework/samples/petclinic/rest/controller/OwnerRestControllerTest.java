2c38d89f60d2620d43dea77853a7fb58ce6becfb
Simone Romani <simone.romani@ing.com>
2025-08-28 14:38:02 +0200

    Add test to verify removal owner

--- src/test/java/org/springframework/samples/petclinic/rest/controller/OwnerRestControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/rest/controller/OwnerRestControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.mapper.OwnerMapper;
+import org.springframework.samples.petclinic.model.Owner;
 import org.springframework.samples.petclinic.rest.dto.OwnerDto;
 import org.springframework.samples.petclinic.rest.dto.OwnerFieldsDto;
 import org.springframework.samples.petclinic.service.ClinicService;
@@ -35,5 +36,18 @@ class OwnerRestControllerTest {
         assertNotNull(response);
         assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
         verify(clinicService, never()).saveOwner(any());
+    }
+
+    @Test
+    void verify_removal_owner() {
+        OwnerFieldsDto fieldsDto = new OwnerFieldsDto();
+        Owner owner = new Owner();
+        owner.setId(1);
+
+        when(ownerMapper.toOwner(fieldsDto)).thenReturn(owner);
+
+        ownerRestController.addOwner(fieldsDto);
+
+        verify(clinicService).saveOwner(owner);
     }
 }
